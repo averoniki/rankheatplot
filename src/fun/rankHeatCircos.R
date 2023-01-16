2b3e11049910c41b963cbe6e57fbfaeea928e405
cklamann <12862284+cklamann@users.noreply.github.com>
2023-01-16 13:56:28 -0500

    cleanup label formatting

--- src/fun/rankHeatCircos.R
+++ src/fun/rankHeatCircos.R
@@ -6,14 +6,14 @@ rankheatplotCircos <- function(data, format = "percentage") {
   data <- as.data.frame(data)
   chartData <- prepData(data)
   rns <- rownames(chartData)
-  chartData[nrow(chartData) + 1,] <- colnames(chartData)
-  rownames(chartData) <- c(rns, "Treatment")
+  chartData[nrow(chartData) + 1, ] <- colnames(chartData)
+  rownames(chartData) <- c(rns, "Outcome")
   pal = brewer.pal(11, "RdYlGn") # will return the hex values
   breaks <-
     seq(0, 100, 10) # assume for now, but need checks later (on format)
   col_func <- colorRamp2(breaks, pal)
- 
-  # wrap color function to swallow string values (i.e., labels) 
+
+  # wrap color function to swallow string values (i.e., labels)
   w_cf <- function(val) {
     clr = 'white'
     clr <- ifelse(!is.na(as.numeric(val)),
@@ -23,8 +23,13 @@ rankheatplotCircos <- function(data, format = "percentage") {
     
   }
   
+  m = nrow(chartData)
+  # offset start so label row (last row) is at the top
+  gapDegree = 2
+  startDegree = 90 - (((360 / m) + gapDegree) / 2)
+
   circos.clear()
-  circos.par(start.degree = 90, gap.degree = 2)
+  circos.par(start.degree = startDegree, gap.degree = gapDegree)
   
   circos.heatmap(
     chartData,
@@ -34,8 +39,7 @@ rankheatplotCircos <- function(data, format = "percentage") {
     cluster = F,
     na.col = "white",
     show.sector.labels = T,
-    track.height = .75,
-
+    track.height = .65,
   )
   
   circos.track(
@@ -43,17 +47,31 @@ rankheatplotCircos <- function(data, format = "percentage") {
     # this will create graphical elements immediately after creation of cell
     panel.fun = function(x, y) {
       n = ncol(chartData)
-      # todo: need min width, smaller font, curve for labels
-      # adjust start angle based on number of rows for better label row positioning
-      circos.text(
-        rep(CELL_META$cell.xlim[2] / 2, n) + convert_x(1, "mm"),
-        1:n - 0.5,
-        rev(chartData[get.cell.meta.data("sector.index"),]),
-        cex = 0.75,
-        adj = c(.5, 0),
-        facing = "inside",
-        niceFacing = T
-      )
+      if (get.cell.meta.data("sector.index") != "Outcome") {
+        circos.text(
+          rep(CELL_META$cell.xlim[2] / 2, n) + convert_x(1, "mm"),
+          1:n - 0.5,
+          rev(chartData[get.cell.meta.data("sector.index"), ]),
+          cex = 0.75,
+          adj = c(.5, .5),
+          facing = "inside",
+          niceFacing = T
+        )
+      } else {
+        # 'bending' requires a loop rather than vector args
+        lbls = rev(names(chartData[get.cell.meta.data("sector.index"), ]))
+        for (i in 1:m) {
+          circos.text(
+            CELL_META$cell.xlim[1],
+            i - .65,
+            lbls[i],
+            cex = 0.75,
+            adj = c(0, .5),
+            facing = "bending",
+            niceFacing = T
+          )
+        }
+      }
     },
     bg.border = NA
   )
@@ -61,12 +79,12 @@ rankheatplotCircos <- function(data, format = "percentage") {
 }
 prepData <- function(df, format = "percentage") {
   # drop rows that don't have a label
-  df <- df[!is.na(df[, 1]), ]
+  df <- df[!is.na(df[, 1]),]
   
   # set first column as row names
   rns <- df[, 1]
   # drop first column
-  df <- df[, -c(1)]
+  df <- df[,-c(1)]
   if (format == "percentage") {
     df <- as.data.frame(sapply(df, function(x)
       x * 100))
